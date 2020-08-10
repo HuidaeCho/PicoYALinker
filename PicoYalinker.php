cf2a4bf28829e1f81b4e899e116c14156a0cd7b1
Huidae Cho <grass4u@gmail.com>
2020-08-09 22:22:23 -0400

    first commit

---
+++ PicoYalinker.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * Pico Yet Another Linker Plugin
+ *
+ * @author Huidae Cho
+ * @link https://github.com/HuidaeCho/pico-yalinker
+ * @license http://opensource.org/licenses/MIT
+ * @version 0.1.0
+ */
+
+class PicoYalinker extends AbstractPicoPlugin {
+
+    const API_VERSION = 3;
+
+    public function onParsedownRegistered(Parsedown &$parsedown)
+    {
+        $pico = $this->getPico();
+        $config = $pico->getConfig();
+
+        // https://example.com/pico => /pico
+        $root_url = preg_replace('/^[^\/]*\/\/[^\/]*(\/.*?)\/*$/', '\1', $config['base_url']);
+        // https://example.com/pico/current-page => /current-page
+        $page_url = $pico->getRequestUrl();
+        // Current Page Title
+        $page_title = $pico->getFileMeta()['title'];
+
+        $utf8_slugs = $config['yalinker_utf8_slugs'];
+
+        $className = ($this->config['content_config']['extra'] ? 'ParsedownExtra' : 'Parsedown').'Yalinker';
+        $parsedown = new $className($utf8_slugs, $root_url, $page_url, $page_title);
+        $parsedown->setBreaksEnabled((bool) $config['content_config']['breaks']);
+        $parsedown->setMarkupEscaped((bool) $config['content_config']['escape']);
+        $parsedown->setUrlsLinked((bool) $config['content_config']['auto_urls']);
+    }
+}
+
+class ParsedownYalinker extends Parsedown
+{
+    protected $utf8_slugs;
+    protected $root_url;
+    protected $page_url;
+    protected $page_title;
+
+    public function __construct($utf8_slugs, $root_url, $page_url, $page_title)
+    {
+        $this->utf8_slugs = $utf8_slugs;
+        $this->root_url = $root_url;
+        $this->page_url = $page_url;
+        $this->page_title = $page_title;
+    }
+
+    protected function inlineLink($excerpt)
+    {
+        $link = parent::inlineLink($excerpt);
+        if ($link === null)
+            $link = yalink($excerpt, $this->utf8_slugs, $this->root_url, $this->page_url, $this->page_title);
+        return $link;
+    }
+}
+
+class ParsedownExtraYalinker extends ParsedownExtra
+{
+    protected $utf8_slugs;
+    protected $root_url;
+    protected $page_url;
+    protected $page_title;
+
+    public function __construct($utf8_slugs, $root_url, $page_url, $page_title)
+    {
+        $this->utf8_slugs = $utf8_slugs;
+        $this->root_url = $root_url;
+        $this->page_url = $page_url;
+        $this->page_title = $page_title;
+    }
+
+    protected function inlineLink($excerpt)
+    {
+        $link = parent::inlineLink($excerpt);
+        if ($link === null)
+            $link = yalink($excerpt, $this->utf8_slugs, $this->root_url, $this->page_url, $this->page_title);
+        return $link;
+    }
+}
+
+function yalink($excerpt, $utf8_slugs, $root_url, $page_url, $page_title)
+{
+    // syntax: [[(link)?(|(text)?)?]]
+    // * [[]] => <a href="/current">Current Title</a>
+    // * [[|]] => <a href="/current">/current</a>
+    // * [[.|]] => <a href="/current">.</a>
+    // * [[page]] => <a href="/current/page">page</a>
+    // * [[page|]] => <a href="/current/page">page</a>
+    // * [[page|text]] => <a href="/current/page">text</a>
+    // * [[../page]] => <a href="/page">page</a>
+    // * [[../page|]] => <a href="/page">../page</a>
+    // * [[../page|text]] => <a href="/page">text</a>
+    // * [[/folder/slashes//in page title]] => <a href="/folder/slashes-in-page-title">slashes/in page title</a>
+    // * [[/folder/slashes//in page title|]] => <a href="/folder/slashes-in-page-title">/folder/slashes/in page title</a>
+    // * [[/folder/slashes//in page title|text]] => <a href="/folder/slashes-in-page-title">text</a>
+    // * [[url]] => <a href="url">url</a>
+    // * [[mailto:...]] => <a href="mailto:...">mailto:...</a>
+    // * [[mail:...]] => <a href="mailto:...">...</a>
+    if (strpos($excerpt['text'], '[[') === 0 &&
+        strpos($excerpt['text'], ']]') !== false &&
+        preg_match('/^\[\[(.*?)(?:\|(.+?))?\]\]/', $excerpt['text'], $matches)) {
+        $extent = strlen($matches[0]);
+        $href = $matches[1];
+        $has_text = isset($matches[2]);
+
+        $show_path = false;
+        if ($has_text)
+            $text = $matches[2];
+        else {
+            $len = strlen($href);
+            if ($len && $href[$len - 1] == '|') {
+                $href = substr($href, 0, $len - 1);
+                $show_path = true;
+            }
+            $text = $href;
+        }
+
+        // if url
+        if (preg_match('/^(?:https?:\/\/|mail(?:to)?:)/', $href))
+            $text = preg_replace('/\|$/', '', $text);
+        else {
+        // if page
+            // escape non-folder slashes
+            if (strpos($href, '//') !== false) {
+                $href = preg_replace('/\/{2,}/', '-', $href);
+                if (!$has_text)
+                    // for now, // => \x00
+                    $text = preg_replace('/\/{2,}/', '\x00', $text);
+            }
+
+            // pre-clean up path in href
+            if (strpos($href, '...') !== false)
+                // a/...../b => a/../b
+                $href = preg_replace('/\.{3,}/', '..', $href);
+            if (strpos($href, '/./') !== false)
+                // a/./././b => a/b
+                $href = preg_replace('/(?:\/\.)+\//', '/', $href);
+            if (strpos($href, './') === 0)
+                // ./a => a
+                $href = substr($href, 2);
+
+            $path_prefix = $page_route;
+            if (preg_match('/^([\/.]*)\/(.*)$/', $href, $matches)) {
+                if (strlen($matches[1]) && $matches[1][0] != '/')
+                    // relative path
+                    $path_prefix .= $matches[1];
+                else
+                    // absolute path
+                    $path_prefix = '';
+                $href = $matches[2];
+            }
+
+            switch ($href) {
+            case '':
+                if (!$has_text)
+                    $text = $show_path ? $path_prefix : $page_title;
+                break;
+            case '.':
+                $href = '';
+                break;
+            }
+
+            // add back path prefix that may has been removed by the slug() function
+            $href = $path_prefix.($href ? '/'.slugify($href, $utf8_slugs) : '');
+
+            // post-clean up path in href
+            $paths = explode('/', $href);
+            $npaths = count($paths);
+            for ($i = 0; $i < $npaths; $i++) {
+                if ($paths[$i] == '..')
+                    $paths[$i - 1] = $paths[$i] = '';
+            }
+            $href = join('/', $paths);
+            $href = $root_url.preg_replace('/\/\/+/', '/', $href);
+
+            if (!$has_text) {
+                // handle page path in text
+                if (!$show_path && preg_match('/^(?:[\/.]*\/)?(?:[^\/]+\/)*(.*)$/', $text, $matches))
+                    // show page title only if requested (no | at the end)
+                    $text = $matches[1];
+
+                // convert escaped non-folder slashes back to slashes
+                $text = str_replace('\x00', '/', $text);
+            }
+        }
+
+        // if mail, hide mailto from text
+        if (strpos($href, 'mail:') === 0) {
+            $href = substr_replace($href, 'mailto:', 0, 5);
+            if (!$has_text)
+                $text = substr_replace($text, '', 0, 5);
+        }
+
+        return [
+            'extent' => $extent,
+            'element' => [
+                'name' => 'a',
+                'text' => $text,
+                'attributes' => [
+                    'href' => $href,
+                ],
+            ],
+        ];
+    }
+}
+
+function slugify($str, $utf8_slugs)
+{
+    if (!$utf8_slugs) {
+        // Adopted from Grav's Admin plugin (user/plugins/admin/classes/utils.php)
+        if (function_exists('transliterator_transliterate')) {
+            $str = transliterator_transliterate('Any-Latin; NFD; [:Nonspacing Mark:] Remove; NFC;', $str);
+        } else {
+            $str = iconv('UTF-8', 'ASCII//TRANSLIT//IGNORE', $str);
+        }
+
+        $str = strtolower($str);
+        $str = preg_replace('/[\s.]+/', '-', $str);
+        // leave slashes as is
+        $str = preg_replace('/[^a-z0-9\/-]/', '', $str);
+    } else {
+        // replace forbidden characters by -
+        // leave slashes as is
+        $str = preg_replace('/[ \t`~!@#\$%^&*=+\\\\|;:\'",?()\[\]{}<>_.]+/', '-', $str);
+        // trim
+        $str = trim($str, '-');
+        // lowercase
+        $str = strtolower($str);
+    }
+
+    // page-/-slug => page/slug
+    $str = preg_replace('/-?\/-?/', '/', $str);
+    $str = trim($str, '-');
+
+    return $str;
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+Pico Yet Another Linker Plugin
+==============================
+
+This Pico plugin adds the ability to create links with less typing in Markdown.
+
+Install
+-------
+
+1. Clone this Github repo into your `plugins/PicoYalinker` directory.
+2. If you want to include UTF-8 characters in slugs, add the following line to `config/config.yml`:
+   ```
+   yalinker_utf8_slugs: true
+   ```
+
+Usage
+-----
+
+| YALink | Markdown |
+| ------ | -------- |
+| `[[Page Title]]` | `[Page Title](page-title)` |
+| `[[Page Title\|Link Text]]` | `[Link Text](page-title)` |
+| `[[Slashes//in Page Title]]` | `[Slashes/in Page Title](slashes-in-page-title)` |
+| `[[../Some Path/Page Title]]` | `[Page Title](../some-path/page-title)` |
+| `[[../Some Path/Page Title\|]]` | `[../Some Path/Page Title](../some-path/page-title)` |
+| `[[/Root Path/Page Title]]` | `[Page Title](/root-path/page-title)` |
+| `[[https://example.com]]` | `<https://example.com>` |
+| `[[mailto:me@example.com]]` | `<mailto:me@example.com>` |
+| `[[mail:me@example.com]]` | `[me@example.com](mailto:me@example.com)` |
+
+Demo
+----
+
+https://demo.isnew.info/pico/markdown-yalinker
+
+Credits
+-------
+
+Author: Huidae Cho
